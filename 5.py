7dbe596b191e282d35df490ea55eb5966bf22f28
Daniel Patru <dpatru@gmail.com>
2017-12-14 07:11:12 -0500

    5b

--- 5.py
+++ 5.py
@@ -1,13 +1,17 @@
 #! /usr/local/bin/python3
 
+# run as: ./5.py < 5.input.txt
+
 xs = []
 
 def jump(i, xs):
     s = 0
     while 0 <= i < len(xs):
         s += 1
-        xs[i] += 1
-        i += xs[i] - 1
+        j = xs[i]
+        # xs[i] += 1 # increase the jump, part 1
+        xs[i] += (1 if j < 3 else -1) # part 2
+        i += j
         # print("i = ", i)
     return s
 
