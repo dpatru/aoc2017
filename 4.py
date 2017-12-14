5dead025e74e95af12160b5ae26df005fbfcb729
Daniel Patru <dpatru@gmail.com>
2017-12-14 07:03:56 -0500

    4b

--- 4.py
+++ 4.py
@@ -1,11 +1,14 @@
 #! /usr/local/bin/python3
 
+# run as ./4.py < 4.passPhrases.txt
+
 valids = 0
 while True: 
     line = input()
     if not line:
         print(valids, " valid passphrases")
         break
-    if len(line.split()) == len(set(line.split())):
+    # if len(line.split()) == len(set(line.split())): # part 1
+    if len(line.split()) == len(set([tuple(sorted(w)) for w in line.split()])): # part 2
         valids += 1
     
